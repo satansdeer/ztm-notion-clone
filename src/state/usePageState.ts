b980beb2dcddd560c1ffc63c91fc03a3802d6c56
Maksim Ivanov <satansdeer@gmail.com>
2023-07-28 12:03:04 +0200

    Define the usePageState hook

---
+++ src/state/usePageState.ts
@@ -0,0 +1,58 @@
+import { Page, NodeData, NodeType } from "../utils/types";
+import { useImmer } from "use-immer";
+
+export const usePageState = (initialState: Page) => {
+  const [page, setPage] = useImmer<Page>(initialState);
+
+  const addNode = (node: NodeData, index: number) => {
+    setPage((draft) => draft.nodes.splice(index, 0, node));
+  };
+
+  const removeNodeByIndex = (nodeIndex: number) => {
+    setPage((draft) => draft.nodes.splice(nodeIndex, 1));
+  };
+
+  const changeNodeValue = (nodeIndex: number, value: string) => {
+    setPage((draft) => {
+      draft.nodes[nodeIndex].value = value;
+    });
+  };
+
+  const changeNodeType = (nodeIndex: number, type: NodeType) => {
+    setPage((draft) => {
+      draft.nodes[nodeIndex].type = type;
+      draft.nodes[nodeIndex].value = "";
+    });
+  };
+
+  const setNodes = (nodes: NodeData[]) => {
+    setPage((draft) => {
+      draft.nodes = nodes;
+    });
+  };
+
+  const setTitle = (title: string) => {
+    setPage((draft) => {
+      draft.title = title;
+    });
+  };
+
+  const setCoverImage = (coverImage: string) => {
+    setPage((draft) => {
+      draft.cover = coverImage;
+    });
+  };
+
+  return {
+	nodes: page.nodes,
+	title: page.title,
+	cover: page.cover,
+	changeNodeType,
+	changeNodeValue,
+	addNode,
+	removeNodeByIndex,
+	setTitle,
+	setCoverImage,
+	setNodes
+  }
+};
