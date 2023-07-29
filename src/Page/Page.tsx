851f59d9268abd99010bd3b58cfd563b99ffda69
Maksim Ivanov <satansdeer@gmail.com>
2023-07-29 15:20:39 +0200

    Make page use appstatecontext

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,36 +1,17 @@
-import { useState } from "react";
-import { NodeData } from "../utils/types";
 import { useFocusedNodeIndex } from "./useFocusedNodeIndex";
 import { Cover } from "./Cover";
 import { Spacer } from "./Spacer";
 import { BasicNode } from "../Node/BasicNode";
 import { Title } from "./Title";
 import { nanoid } from "nanoid";
+import { useAppState } from "../state/AppStateContext"
 
 export const Page = () => {
-  const [nodes, setNodes] = useState<NodeData[]>([]);
-  const [title, setTitle] = useState("Default Title");
+	const {title, nodes, addNode, setTitle} = useAppState()
+
   const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({
     nodes,
   });
-
-  const addNode = (node: NodeData, index: number) => {
-    const newNodes = [...nodes];
-    newNodes.splice(index, 0, node);
-    setNodes(newNodes);
-  };
-
-  const removeNodeByIndex = (index: number) => {
-    const newNodes = [...nodes];
-    newNodes.splice(index, 1);
-    setNodes(newNodes);
-  };
-
-  const changeNodeValue = (index: number, value: string) => {
-    const newNodes = [...nodes];
-    newNodes[index].value = value;
-    setNodes(newNodes);
-  };
 
   return (
     <>
@@ -44,9 +25,6 @@ export const Page = () => {
             isFocused={focusedNodeIndex === index}
             updateFocusedIndex={setFocusedNodeIndex}
             index={index}
-            addNode={addNode}
-            removeNodeByIndex={removeNodeByIndex}
-            changeNodeValue={changeNodeValue}
           />
         ))}
         <Spacer
