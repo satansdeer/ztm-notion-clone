960be5a12666116f0bd6613a1433bed66a1d33ef
Maksim Ivanov <satansdeer@gmail.com>
2023-07-29 19:46:12 +0200

    Add node type switcher

---
+++ src/Node/NodeTypeSwitcher.tsx
@@ -0,0 +1,31 @@
+import { NodeType, NodeData } from "../utils/types"
+import { BasicNode } from "./BasicNode"
+
+type NodeTypeSwitcherProps = {
+  node: NodeData;
+  updateFocusedIndex(index: number): void;
+  isFocused: boolean;
+  index: number;
+};
+
+const TEXT_NODE_TYPES: NodeType[] = [
+"text", "list", "heading1", "heading2", "heading3"
+]
+
+export const NodeTypeSwitcher = ({
+	node,
+	isFocused,
+	index,
+	updateFocusedIndex
+}: NodeTypeSwitcherProps) => {
+	if(TEXT_NODE_TYPES.includes(node.type)) {
+		return <BasicNode
+			node={node}
+			index={index}
+			isFocused={isFocused}
+			updateFocusedIndex={updateFocusedIndex}
+		/>
+	}
+
+	return null
+}
