74ae9682e5274a7509f2c0fe9b5867358b67bf70
Maksim Ivanov <satansdeer@gmail.com>
2023-07-29 20:33:22 +0200

    Add command panel

---
+++ src/Node/CommandPanel.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { NodeType } from "../utils/types";
+import { useOverflowsScreenBottom } from "./useOverflowsScreenBottom";
+import styles from "./CommandPanel.module.css";
+import cx from "classnames";
+
+type CommandPanelProps = {
+  nodeText: string;
+  selectItem: (nodeType: NodeType) => void;
+};
+
+type SupportedNodeType = {
+  value: NodeType;
+  name: string;
+};
+
+const supportedNodeTypes: SupportedNodeType[] = [
+  { value: "text", name: "Text" },
+  { value: "list", name: "Lext" },
+  { value: "heading1", name: "Heading 1" },
+  { value: "heading2", name: "Heading 2" },
+  { value: "heading3", name: "Heading 3" },
+];
+
+export const CommandPanel = ({ selectItem, nodeText }: CommandPanelProps) => {
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+  const { overflows, ref } = useOverflowsScreenBottom();
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        selectItem(supportedNodeTypes[selectedItemIndex].value);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [selectedItemIndex, selectItem]);
+
+  useEffect(() => {
+    const normalizedValue = nodeText.toLowerCase().replace(/\//, "");
+    setSelectedItemIndex(
+      supportedNodeTypes.findIndex((item) => item.value.match(normalizedValue))
+    );
+  }, [nodeText]);
+
+  return (
+    <div
+      ref={ref}
+      className={cx(styles.panel, {
+        [styles.reverse]: overflows,
+      })}
+    >
+      <div className={styles.title}>Blocks</div>
+      <ul>
+        {supportedNodeTypes.map((type, index) => {
+          const selected = selectedItemIndex === index;
+
+          return (
+            <li
+              key={type.value}
+              className={cx({
+                [styles.selected]: selected,
+              })}
+              onClick={() => selectItem(type.value)}
+            >
+              {type.name}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
