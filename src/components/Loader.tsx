0544c99684cda20b0d6fccf4e115c825eb05424a
Maksim Ivanov <satansdeer@gmail.com>
2023-07-30 20:43:23 +0200

    Add loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,14 @@
+import styles from "./Loader.module.css";
+
+// https://loading.io/css/
+
+export const Loader = () => {
+	return (
+		<div className={styles.loader}>
+			<div />
+			<div />
+			<div />
+			<div />
+		</div>
+	);
+};
