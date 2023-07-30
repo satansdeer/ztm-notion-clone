073aeab31974518c90a86004016dcdfb7cb28673
Maksim Ivanov <satansdeer@gmail.com>
2023-07-30 14:05:00 +0200

    Install dnd-kit

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,7 +1,7 @@
 import { useFocusedNodeIndex } from "./useFocusedNodeIndex";
 import { Cover } from "./Cover";
 import { Spacer } from "./Spacer";
-import { BasicNode } from "../Node/BasicNode";
+import { NodeTypeSwitcher } from "../Node/NodeTypeSwitcher";
 import { Title } from "./Title";
 import { nanoid } from "nanoid";
 import { useAppState } from "../state/AppStateContext"
@@ -19,7 +19,7 @@ export const Page = () => {
       <div>
         <Title addNode={addNode} title={title} changePageTitle={setTitle} />
         {nodes.map((node, index) => (
-          <BasicNode
+          <NodeTypeSwitcher
             key={node.id}
             node={node}
             isFocused={focusedNodeIndex === index}
