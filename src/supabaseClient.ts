868980036210151fd0c57c85f15e73ae597126b9
Maksim Ivanov <satansdeer@gmail.com>
2023-08-03 08:33:42 +0200

    add supabase cli

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_API_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
 if(!supabaseUrl || !supabaseKey){
 	throw new Error("Missing supabase url or key")
