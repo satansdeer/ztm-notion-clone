b0013f7aca64b533f21104206578504b5c785d53
Maksim Ivanov <satansdeer@gmail.com>
2024-04-07 17:14:01 +0200

    Fix a bug that prevented new pages creation

--- src/state/usePageState.ts
+++ src/state/usePageState.ts
@@ -2,6 +2,7 @@ import { Page, NodeData, NodeType } from "../utils/types";
 import { arrayMove } from "@dnd-kit/sortable"
 import { useSyncedState } from "./useSyncedState"
 import { updatePage } from "../utils/updatePage"
+import { createPage } from "../utils/createPage"
 
 export const usePageState = (initialState: Page) => {
   const [page, setPage] = useSyncedState(initialState, updatePage);
@@ -24,11 +25,21 @@ export const usePageState = (initialState: Page) => {
     });
   };
 
-  const changeNodeType = (nodeIndex: number, type: NodeType) => {
-    setPage((draft) => {
-      draft.nodes[nodeIndex].type = type;
-      draft.nodes[nodeIndex].value = "";
-    });
+  const changeNodeType = async (nodeIndex: number, type: NodeType) => {
+    if (type === "page") {
+      const newPage = await createPage();
+      if (newPage) {
+        setPage((draft) => {
+          draft.nodes[nodeIndex].type = type;
+          draft.nodes[nodeIndex].value = newPage.slug;
+        });
+      }
+    } else {
+      setPage((draft) => {
+        draft.nodes[nodeIndex].type = type;
+        draft.nodes[nodeIndex].value = "";
+      });
+    }
   };
 
   const setNodes = (nodes: NodeData[]) => {
@@ -49,13 +60,13 @@ export const usePageState = (initialState: Page) => {
     });
   };
 
-	const reorderNodes = (id1: string, id2: string) => {
-		setPage((draft) => {
-			const index1 = draft.nodes.findIndex(node => node.id === id1)
-			const index2 = draft.nodes.findIndex(node => node.id === id2)
-			draft.nodes = arrayMove(draft.nodes, index1, index2)
-		})
-	}
+  const reorderNodes = (id1: string, id2: string) => {
+    setPage((draft) => {
+      const index1 = draft.nodes.findIndex(node => node.id === id1)
+      const index2 = draft.nodes.findIndex(node => node.id === id2)
+      draft.nodes = arrayMove(draft.nodes, index1, index2)
+    })
+  }
 
   return {
     nodes: page.nodes,
@@ -68,6 +79,6 @@ export const usePageState = (initialState: Page) => {
     setTitle,
     setCoverImage,
     setNodes,
-		reorderNodes
+    reorderNodes
   };
 };
--- src/utils/createPage.ts
+++ src/utils/createPage.ts
@@ -1,16 +1,31 @@
 import { nanoid } from "nanoid";
-import { Page } from "./types"
+import { supabase } from "../supabaseClient";
 
-export const createPage = () => {
+export const createPage = async () => {
+	const { data: userData } = await supabase.auth.getUser();
+	const user = userData.user;
+	if (!user) {
+		throw new Error("You must be logged in to create a page.");
+	}
 	const slug = nanoid();
-	const id = nanoid()
 
-	const page: Page = {
+	const page = {
+		id: undefined,
 		title: "Untitled",
-		id,
 		slug,
 		nodes: [],
-		cover: "ztm-cover.png"
-	}
-	return page
-}
+		created_by: user.id,
+	};
+
+	await supabase.from("pages").insert(page);
+	const { data: pageData } = await supabase
+		.from("pages")
+		.select("id")
+		.match({ slug, created_by: user.id })
+		.single();
+
+	page.id = pageData?.id;
+
+	return page;
+};
+
