83f407169bf413804cd2f0af07278d500e85120f
Maksim Ivanov <satansdeer@gmail.com>
2024-04-07 17:18:58 +0200

    Fix initial state calculation

--- src/state/withInitialState.tsx
+++ src/state/withInitialState.tsx
@@ -1,6 +1,6 @@
 import { Page } from "../utils/types";
 import { useMatch } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { supabase } from "../supabaseClient";
 import startPageScaffold from "./startPageScaffold.json";
 import styles from "../utils.module.css";
@@ -24,9 +24,14 @@ export function withInitialState<TProps>(
     const [initialState, setInitialState] = useState<Page | null>();
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<Error | undefined>();
+    const inProgress = useRef(false)
 
     useEffect(() => {
+      if (inProgress.current) {
+        return
+      }
       setIsLoading(true);
+      inProgress.current = true
       const fetchInitialState = async () => {
         try {
           const { data: userData } = await supabase.auth.getUser();
@@ -38,25 +43,38 @@ export function withInitialState<TProps>(
             .from("pages")
             .select("title, id, cover, nodes, slug")
             .match({ slug: pageSlug, created_by: user.id })
-            .single();
-          if (!data && pageSlug === "start") {
-            const result = await supabase
+
+          if (data?.[0]) {
+            setInitialState(data?.[0]);
+            inProgress.current = false;
+            setIsLoading(false);
+            return
+          }
+
+          if (pageSlug === "start") {
+            await supabase
               .from("pages")
               .insert({
                 ...startPageScaffold,
                 slug: "start",
                 created_by: user.id,
               })
-              .single();
-            setInitialState(result?.data);
+
+            const { data } = await supabase
+              .from("pages")
+              .select("title, id, cover, nodes, slug")
+              .match({ slug: "start", created_by: user.id })
+
+            setInitialState(data?.[0]);
           } else {
-            setInitialState(data);
+            setInitialState(data?.[0]);
           }
         } catch (e) {
           if (e instanceof Error) {
             setError(e);
           }
         }
+        inProgress.current = false;
         setIsLoading(false);
       };
       fetchInitialState();
