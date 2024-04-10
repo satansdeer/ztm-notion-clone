fafe304245c18f4789d03fb43d738a9c07d29057
arianna choza <unachoza@gmail.com>
2024-04-10 16:25:46 -0400

    added arrow up and arrow down functionality to command panel on key press

--- src/Node/CommandPanel.tsx
+++ src/Node/CommandPanel.tsx
@@ -5,75 +5,83 @@ import styles from "./CommandPanel.module.css";
 import cx from "classnames";
 
 type CommandPanelProps = {
-  nodeText: string;
-  selectItem: (nodeType: NodeType) => void;
+	nodeText: string;
+	selectItem: (nodeType: NodeType) => void;
 };
 
 type SupportedNodeType = {
-  value: NodeType;
-  name: string;
+	value: NodeType;
+	name: string;
 };
 
 const supportedNodeTypes: SupportedNodeType[] = [
-  { value: "text", name: "Text" },
-  { value: "list", name: "List" },
-  { value: "page", name: "Page" },
-  { value: "image", name: "Image" },
-  { value: "heading1", name: "Heading 1" },
-  { value: "heading2", name: "Heading 2" },
-  { value: "heading3", name: "Heading 3" },
+	{ value: "text", name: "Text" },
+	{ value: "list", name: "List" },
+	{ value: "page", name: "Page" },
+	{ value: "image", name: "Image" },
+	{ value: "heading1", name: "Heading 1" },
+	{ value: "heading2", name: "Heading 2" },
+	{ value: "heading3", name: "Heading 3" },
 ];
 
 export const CommandPanel = ({ selectItem, nodeText }: CommandPanelProps) => {
-  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
-  const { overflows, ref } = useOverflowsScreenBottom();
+	const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+	const { overflows, ref } = useOverflowsScreenBottom();
 
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Enter") {
-        selectItem(supportedNodeTypes[selectedItemIndex].value);
-      }
-    };
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === "Enter") {
+				selectItem(supportedNodeTypes[selectedItemIndex].value);
+			}
 
-    window.addEventListener("keydown", handleKeyDown);
+			if (event.key === "ArrowUp") {
+				if (selectedItemIndex === 0) return;
+				setSelectedItemIndex((prevSelectedItemIndex) => prevSelectedItemIndex - 1);
+			}
 
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [selectedItemIndex, selectItem]);
+			if (event.key === "ArrowDown") {
+				if (selectedItemIndex === supportedNodeTypes.length - 1) return;
+				setSelectedItemIndex((prevSelectedItemIndex) => prevSelectedItemIndex + 1);
+			}
+		};
 
-  useEffect(() => {
-    const normalizedValue = nodeText.toLowerCase().replace(/\//, "");
-    setSelectedItemIndex(
-      supportedNodeTypes.findIndex((item) => item.value.match(normalizedValue))
-    );
-  }, [nodeText]);
+		window.addEventListener("keydown", handleKeyDown);
 
-  return (
-    <div
-      ref={ref}
-      className={cx(styles.panel, {
-        [styles.reverse]: overflows,
-      })}
-    >
-      <div className={styles.title}>Blocks</div>
-      <ul>
-        {supportedNodeTypes.map((type, index) => {
-          const selected = selectedItemIndex === index;
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+	}, [selectedItemIndex, selectItem]);
 
-          return (
-            <li
-              key={type.value}
-              className={cx({
-                [styles.selected]: selected,
-              })}
-              onClick={() => selectItem(type.value)}
-            >
-              {type.name}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+	useEffect(() => {
+		const normalizedValue = nodeText.toLowerCase().replace(/\//, "");
+		setSelectedItemIndex(supportedNodeTypes.findIndex((item) => item.value.match(normalizedValue)));
+	}, [nodeText]);
+
+	return (
+		<div
+			ref={ref}
+			className={cx(styles.panel, {
+				[styles.reverse]: overflows,
+			})}
+		>
+			<div className={styles.title}>Blocks</div>
+			<ul>
+				{supportedNodeTypes.map((type, index) => {
+					const selected = selectedItemIndex === index;
+
+					return (
+						<li
+							key={type.value}
+							className={cx({
+								[styles.selected]: selected,
+							})}
+							onClick={() => selectItem(type.value)}
+						>
+							{type.name}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
